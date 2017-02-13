cfb68e82298df122967b9a9a115c8deee0943295
Moony <bradenanelson@gmail.com>
2017-02-13 17:43:00 +0000

    Add a haskell sandbox, https://hackage.haskell.org/package/mueval USE THE MANUAL SETUP, NOT THE CABAL SETUP Requires the package 'haskell-platform' to be installed on your system.

---
+++ commands/hseval/hseval.js
@@ -0,0 +1,25 @@
+const spawn = require('child_process').spawn;
+
+function hseval(code, cb) {
+  let out = '';
+  const mueval = spawn('mueval', ['--inferred-type','-e', code]);
+  mueval.stdout.on('data', (d) => {
+    out += d.toString();
+  });
+  mueval.stderr.on('data', (d) => {
+    out += d.toString();
+  });
+  mueval.on('exit', () => {
+    cb(out);
+  });
+}
+module.exports = {
+  usage: 'hseval <haskell expression> - runs <haskell expression> using mueval as the sandbox',
+  run: (bot, message, args) => {
+    if (!args.length) return true;
+    hseval(args, (res) => {
+      message.channel.sendMessage(`\`\`\`${res}\`\`\``);
+    });
+    return false;
+  },
+};
